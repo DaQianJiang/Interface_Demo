a38e8e2c77e664dafd35bef7992dc83101a16b53
DaQianJiang <email@example.com1404482005@qq.com>
2017-12-27 13:47:30 +0800

    add new file

--- interface_demo/test/suit/test_runner.py
+++ interface_demo/test/suit/test_runner.py
@@ -1,10 +1,14 @@
 import unittest
 import os
-#from test.case.sign_in_test import Sign_In_Test
+from utils.log import ROOT_PATH
+from utils.HTMLTestRunner_PY3 import HTMLTestRunner
 
+report_name = os.path.join(ROOT_PATH,'report\\report.html')
+fp = open(report_name,'wb')
 case_path = os.path.dirname(os.path.abspath('.'))+r'\case'
-#print(case_path)
 suite = unittest.TestLoader().discover(case_path)
 if __name__=="__main__":
-    runner = unittest.TextTestRunner()
+
+    #runner = unittest.TextTestRunner()
+    runner = HTMLTestRunner(stream=fp,verbosity=2,title='接口测试报告',description='网站接口')
     runner.run(suite)
--- interface_demo/utils/log.py
+++ interface_demo/utils/log.py
@@ -3,15 +3,16 @@ import os
 from  logging.handlers import TimedRotatingFileHandler
 import time
 
-ROOT_PATH = os.path.dirname(os.path.abspath('.'))
+ROOT_PATH = os.path.split(os.path.dirname(os.path.abspath('.')))[0]
 LOGGER_PATH = os.path.join(ROOT_PATH,'log')
 
 class Logger(object):
     def __init__(self,logger_name):
         nowtime = time.strftime('%Y%m%d')
         self.file_name = os.path.join(LOGGER_PATH,nowtime+r'.log')
+        #print(self.file_name)
         self.logger = logging.getLogger(logger_name) #创建一个logger对象
-        self.logger.setLevel(logging.DEBUG) #设置日志级别为debug,则比debug大的都能输出
+        self.logger.setLevel(logging.DEBUG) #设置日志级别为debug,则比debug级别高的都能输出
 
         self.console_handler = logging.StreamHandler()
         self.console_handler.setLevel(logging.DEBUG) #比debug低的日志级别忽略不输出
@@ -25,9 +26,9 @@ class Logger(object):
         self.logger.addHandler(self.console_handler)
         self.logger.addHandler(self.file_handler)
         return self.logger
-if __name__=="__main__":
-    log1 = Logger('test_logger').get_logger()
-    log1.debug("debuge")
-    log1.warning("woring")
-    log1.info("info")
+# if __name__=="__main__":
+#     log1 = Logger('test_logger').get_logger()
+#     log1.debug("debuge")
+#     log1.warning("woring")
+#     log1.info("info")
 
